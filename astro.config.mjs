994a960d0f6d4091d99b8b162468cbd154d9d5a1
Himel Majumder <himelmajumder1@gmail.com>
2026-02-02 16:04:22 +0100

    Enhance Astro configuration with Tailwind CSS and MDX support, update dependencies, and refactor index page structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
+import tailwindcss from '@tailwindcss/vite'
+
+import mdx from '@astrojs/mdx'
+import remarkToc from 'remark-toc'
+import rehypePresetMinify from 'rehype-preset-minify'
+import react from '@astrojs/react';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: 'github-dark' },
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypePresetMinify],
+    remarkRehype: { footnoteLabel: 'Footnotes' },
+    gfm: false
+  }), react()]
+})

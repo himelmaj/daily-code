e20a305c13af76417bbc46a10dd621ad86c23174
Himel Majumder <himelmajumder1@gmail.com>
2026-02-03 21:21:43 +0100

    Refactor day-3.mdx content to update title and description; remove description field from config schema; enhance [slug].astro layout for improved slug display.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,6 @@ const daysCollection = defineCollection({
     type: 'content',
     schema: z.object({
         title: z.string(),
-        description: z.string(),
         date: z.coerce.date()
     })
 })
